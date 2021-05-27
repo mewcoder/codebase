49eeb9c39c756046854ba06a5cff45926999055a
mewcoder <1010953107@qq.com>
2021-05-27 22:36:25 +0800

    [add]upload

--- zhihu/src/App.vue
+++ zhihu/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <global-header :user="currentUser"></global-header>
     <column-list :list="list"></column-list>
   </div>
 </template>
@@ -9,6 +10,9 @@ import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+
+// 假数据
 const img =
   'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fdiy.qqjay.com%2Fu%2Ffiles%2F2012%2F0817%2F94de8c23ff7c45c92344cb3705d74f8e.jpg&refer=http%3A%2F%2Fdiy.qqjay.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1624637005&t=8e0fe028ba553cfcf1a943b5163ee876'
 const testData: ColumnProps[] = [
@@ -49,14 +53,22 @@ const testData: ColumnProps[] = [
   }
 ]
 
+const user: UserProps = {
+  isLogin: true,
+  name: 'admin',
+  id: 1
+}
+
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList
+    ColumnList,
+    GlobalHeader
   },
   setup () {
     return {
-      list: testData
+      list: testData,
+      currentUser: user
     }
   }
 })
---
+++ zhihu/src/components/Dropdown.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="dropdown" ref="dropdownRef">
+    <a
+      href="#"
+      class="btn btn-outline-light my-2 dropdown-toggle"
+      @click.prevent="toggleOpen"
+    >
+      {{ title }}
+    </a>
+    <ul class="dropdown-menu" :style="{ display: 'block' }" v-if="isOpen">
+      <slot></slot>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, watch } from 'vue'
+import useClickOutside from './useClickOutside'
+export default defineComponent({
+  name: 'Dropdown',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  setup () {
+    const isOpen = ref(false)
+    // 设置并返回.拿到ref节点
+    const dropdownRef = ref<null | HTMLElement>(null)
+    const toggleOpen = () => {
+      isOpen.value = !isOpen.value
+    }
+    const isClickOutside = useClickOutside(dropdownRef)
+
+    watch(isClickOutside, () => {
+      if (isOpen.value && isClickOutside.value) {
+        isOpen.value = false
+      }
+    })
+    return {
+      isOpen,
+      toggleOpen,
+      dropdownRef // ref使用
+    }
+  }
+})
+</script>
+
+<style></style>
